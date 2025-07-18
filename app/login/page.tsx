36d72e702b85cedaca0cd5f82ba00acaf790a93e
avwarrier <avwarrier@ucdavis.edu>
2025-07-18 00:46:53 -0700

    linting off

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,54 +1,54 @@
-"use client"
+// "use client"
 
-import { supabase } from "@/lib/supabaseClient";
-import React, { useEffect, useState } from "react";
-import { Session } from "@supabase/supabase-js";
+// import { supabase } from "@/lib/supabaseClient";
+// import React, { useEffect, useState } from "react";
+// import { Session } from "@supabase/supabase-js";
 
-export default function Home() {
+// export default function Home() {
 
-    const [session, setSession] = useState<Session | null>(null)
-    useEffect(() => {
-        supabase.auth.getSession().then(({ data: { session } }) => {
-            setSession(session)
-        })
-        const {
-            data: { subscription },
-        } = supabase.auth.onAuthStateChange((_event, session) => {
-            setSession(session)
-        })
-        return () => subscription.unsubscribe()
-    }, [])
+//     const [session, setSession] = useState<Session | null>(null)
+//     useEffect(() => {
+//         supabase.auth.getSession().then(({ data: { session } }) => {
+//             setSession(session)
+//         })
+//         const {
+//             data: { subscription },
+//         } = supabase.auth.onAuthStateChange((_event, session) => {
+//             setSession(session)
+//         })
+//         return () => subscription.unsubscribe()
+//     }, [])
 
-    const signOut = async () => {
-        const { error } = await supabase.auth.signOut()
-        if (error) {
-            console.error(error)
-        }
-    }
+//     const signOut = async () => {
+//         const { error } = await supabase.auth.signOut()
+//         if (error) {
+//             console.error(error)
+//         }
+//     }
 
-    const signInWithGoogle = async () => {
-        const { data, error } = await supabase.auth.signInWithOAuth({
-            provider: "google",
-            options: {
-                redirectTo: `${window.location.origin}/home`
-            }
-        })
-        console.log(data, error)
-    }
+//     const signInWithGoogle = async () => {
+//         const { data, error } = await supabase.auth.signInWithOAuth({
+//             provider: "google",
+//             options: {
+//                 redirectTo: `${window.location.origin}/home`
+//             }
+//         })
+//         console.log(data, error)
+//     }
 
-    if (!session) {
-      return (
-        <div>
-          <button onClick={signInWithGoogle}>Sign in with Google</button>
-        </div>
-      )
-    }
-    else {
-      return (
-        <div>
-          <button onClick={signOut}>Sign out</button>
-          <p>Logged in as {session.user.email}</p>
-        </div>
-      )
-  }
-}
+//     if (!session) {
+//       return (
+//         <div>
+//           <button onClick={signInWithGoogle}>Sign in with Google</button>
+//         </div>
+//       )
+//     }
+//     else {
+//       return (
+//         <div>
+//           <button onClick={signOut}>Sign out</button>
+//           <p>Logged in as {session.user.email}</p>
+//         </div>
+//       )
+//   }
+// }
